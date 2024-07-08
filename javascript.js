55c268a5fb431a29e168364f68ca74dbca6a5587
Leandro <63678585+Liuth16@users.noreply.github.com>
2024-07-08 03:35:51 -0300

    Finish play loop to play 5 times.

--- javascript.js
+++ javascript.js
@@ -1,13 +1,54 @@
-console.log("This is a test")
+// let userChoice = prompt("Make your choice: ");
+// userChoice = userChoice.toLowerCase();
+
+// console.log(userChoice);
+
+let humanScore = 0
+let computerScore = 0
 
 function getComputerChoice(){
-    let randomNumber = Math.floor(Math.random()*100)
-    console.log(randomNumber)
+    let randomNumber = Math.floor(Math.random()*100);
     if (randomNumber < 33){
-        return "rock"
+        return "rock";
     } else if (randomNumber < 66){
-        return "paper"
+        return "paper";
     } else{
-        return "scissor"
+        return "scissor";
     }
+}
+
+function getHumanChoice(){
+    let userChoice = prompt("Make your choice: ");
+    userChoice = userChoice.toLowerCase();
+    return userChoice;
+}
+
+function playRound(humanChoice = getHumanChoice(), computerChoice = getComputerChoice()){
+    console.log(humanChoice)
+    console.log(computerChoice)
+
+    if (humanChoice === computerChoice){
+        console.log(`Both sides selected ${humanChoice}. This is a tie!`)
+        console.log(`Your score: ${humanScore}. Computer score: ${computerScore}`)
+    } else if (humanChoice === "rock" && computerChoice === "scissor"){
+        console.log(`You win! ${humanChoice} beats ${computerChoice}.`)
+        humanScore+=1
+        console.log(`Your score: ${humanScore}. Computer score: ${computerScore}`)
+    } else if (humanChoice === "paper" && computerChoice === "rock"){
+        console.log(`You win! ${humanChoice} beats ${computerChoice}.`)
+        humanScore+=1
+        console.log(`Your score: ${humanScore}. Computer score: ${computerScore}`)
+    } else if (humanChoice === "scissor" && computerChoice === "paper"){
+        console.log(`You win! ${humanChoice} beats ${computerChoice}.`)
+        humanScore+=1
+        console.log(`Your score: ${humanScore}. Computer score: ${computerScore}`)
+    } else {
+        console.log(`You lose! ${computerChoice} beats ${humanChoice}.`)
+        computerScore+=1
+        console.log(`Your score: ${humanScore}. Computer score: ${computerScore}`)
+    }
+}
+
+for (let i = 0; i < 5; i++){
+    playRound()
 }
