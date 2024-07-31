842dc1d0d281eb92cf20f17f25b02f7e3c780e0f
Leandro <63678585+Liuth16@users.noreply.github.com>
2024-07-31 20:53:55 -0300

    Add UI and buttons to the game

--- javascript.js
+++ javascript.js
@@ -2,6 +2,8 @@
 
 let humanScore = 0
 let computerScore = 0
+let winnerDeclared = false;
+let winner = '';
 
 function getComputerChoice(){
     let randomNumber = Math.floor(Math.random()*100);
@@ -14,36 +16,76 @@ function getComputerChoice(){
     }
 }
 
-function getHumanChoice(){
-    let userChoice = prompt("Make your choice: ");
+function getHumanChoice(choice){
+    let userChoice = choice;
     userChoice = userChoice.toLowerCase();
     return userChoice;
 }
 
-function playRound(humanChoice = getHumanChoice(), computerChoice = getComputerChoice()){
+function checkWinner(){
+    if (humanScore > 4 && !winnerDeclared){
+        winner = "human";
+        winnerDeclared = true;
+    } else if (computerScore > 4 && !winnerDeclared) {
+        winner = "machine";
+        winnerDeclared = true;
+    }
 
-    if (humanChoice === computerChoice){
-        console.log(`Both sides selected ${humanChoice}. This is a tie!`)
-        console.log(`Your score: ${humanScore}. Computer score: ${computerScore}`)
-    } else if (humanChoice === "rock" && computerChoice === "scissor"){
-        console.log(`You win! ${humanChoice} beats ${computerChoice}.`)
-        humanScore+=1
-        console.log(`Your score: ${humanScore}. Computer score: ${computerScore}`)
-    } else if (humanChoice === "paper" && computerChoice === "rock"){
-        console.log(`You win! ${humanChoice} beats ${computerChoice}.`)
-        humanScore+=1
-        console.log(`Your score: ${humanScore}. Computer score: ${computerScore}`)
-    } else if (humanChoice === "scissor" && computerChoice === "paper"){
-        console.log(`You win! ${humanChoice} beats ${computerChoice}.`)
-        humanScore+=1
-        console.log(`Your score: ${humanScore}. Computer score: ${computerScore}`)
-    } else {
-        console.log(`You lose! ${computerChoice} beats ${humanChoice}.`)
-        computerScore+=1
-        console.log(`Your score: ${humanScore}. Computer score: ${computerScore}`)
+    if (humanScore > 4 || computerScore > 4){
+
+        const header = document.createElement("h3");
+        header.textContent = `And the winner is the ${winner}! `;
+        div.appendChild(header);
     }
 }
 
-for (let i = 0; i < 5; i++){
-    playRound()
+const div = document.querySelector("#results");
+const paragraph = document.createElement("p");
+
+function playRound(humanChoice = getHumanChoice(), computerChoice = getComputerChoice()){
+
+    if (humanChoice === computerChoice){
+        div.textContent = `Both sides selected ${humanChoice}. This is a tie!`
+        paragraph.textContent = `Your score: ${humanScore}. Computer score: ${computerScore}`;
+        div.appendChild(paragraph);
+    } else if (humanChoice === "rock" && computerChoice === "scissor"){
+        humanScore+=1
+        div.textContent = `You win! ${humanChoice} beats ${computerChoice}.`
+        paragraph.textContent = `Your score: ${humanScore}. Computer score: ${computerScore}`;
+        div.appendChild(paragraph);
+    } else if (humanChoice === "paper" && computerChoice === "rock"){
+        humanScore+=1
+        div.textContent = `You win! ${humanChoice} beats ${computerChoice}.`
+        paragraph.textContent = `Your score: ${humanScore}. Computer score: ${computerScore}`;
+        div.appendChild(paragraph);
+    } else if (humanChoice === "scissor" && computerChoice === "paper"){
+        humanScore+=1
+        div.textContent = `You win! ${humanChoice} beats ${computerChoice}.`
+        paragraph.textContent = `Your score: ${humanScore}. Computer score: ${computerScore}`;
+        div.appendChild(paragraph);
+    } else {
+        computerScore+=1
+        div.textContent = `You lose! ${computerChoice} beats ${humanChoice}.`
+        paragraph.textContent = `Your score: ${humanScore}. Computer score: ${computerScore}`;
+        div.appendChild(paragraph);
+    }
+
+    checkWinner();
 }
+
+const menu = document.querySelector("#menu");
+menu.addEventListener("click", (e) =>{
+let target = e.target;
+switch(target.id){
+    case 'rock':
+        playRound(getHumanChoice('rock'),);
+        break;
+    case 'paper':
+        playRound(getHumanChoice('paper'),);
+        break;
+    case 'scissor':
+    playRound(getHumanChoice('scissor'),);
+    break;
+}
+})
+
